f68b5f65bc4d820c7091f31aa62f5b3ac64bcd36
CameronLee06 <99371789+CameronLee06@users.noreply.github.com>
2022-02-18 11:33:42 +1300

    working simple yes / no

--- 01_yes_no_v1.py
+++ 01_yes_no_v1.py
@@ -1,19 +1,24 @@
-# Ask the user if they have played before
-show_instructions = input("Have you played this game before?" ).lower()
 
-# If they say yes, output 'program continues'
-if show_instructions == "yes":
-    print("program continues")
+show_instructions = ""
+while show_instructions.lower() != "xxx":
+    # Ask the user if they have played before
+    show_instructions = input("Have you played this game before?" ).lower()
 
-elif show_instructions == "y":
-    print("program continues")
-    
-elif show_instructions == "n":
+    # If they say yes, output 'program continues'
+    # If they say no, output 'display instructions'
+    # If the answer is invalid, print an error.
+
+    if show_instructions == "yes":
+        print("program continues")
+
+    elif show_instructions == "y":
+        print("program continues")
+
+    elif show_instructions == "n":
         print("display instructions")
 
-elif show_instructions == "no":
-    print("display instructions") 
+    elif show_instructions == "no":
+        print("display instructions") 
 
-# If they say no, output 'display instructions'
-else:
-    print("Please answer yes / no")
+    else:
+        print("Please answer yes / no")
