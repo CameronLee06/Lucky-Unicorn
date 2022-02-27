8f1793c26891d5852b68861c5c6cd1de591c5008
CameronLee06 <99371789+CameronLee06@users.noreply.github.com>
2022-02-28 10:55:25 +1300

    Add files via upload

--- 03_Instructions_v1.py
+++ 03_Instructions_v1.py
@@ -15,6 +15,14 @@ def yes_no(question):
             print("Please answer yes / no")
 
 
+def instructions():
+    print("**** How To Play ****")
+    print()
+    print("The rules of the game go here")
+    print()
+    return ""
+
+
 show_instructions = ""
 while show_instructions.lower() != "xxx":
     # Ask the user if they have played before
@@ -25,4 +33,7 @@ while show_instructions.lower() != "xxx":
     elif show_instructions == "no":
         print("show instructions \n")
 
-    
+if show_instructions == "yes":
+    instructions()
+else:
+    print("Program Continues")
---
+++ 04_how_much_v2.py
@@ -0,0 +1,27 @@
+# Functions go here
+def num_check(question, low, high):
+    error = "please enter an whole number between 1 and 10\n"
+
+    valid = False
+    while not valid:
+        try:
+            # ask the question
+            response = int(input("How much would you like to play with?"))
+
+            # if the amount is too low / too high give
+            if 0 < response <= 10:
+                return response
+
+
+                # output an error
+            else: 
+                print(error)
+
+        except ValueError:
+            print("Error, please try again")
+
+
+# *** Main Routine ****
+
+how_much = num_check("How much do you want to play with? ", 1, 10)
+print("you chose to play with ${:.2f}".format(how_much))
