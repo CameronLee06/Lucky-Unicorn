805d03e1c2a73f8bdcfbf668f5f067f7eec6a56c
CameronLee06 <99371789+CameronLee06@users.noreply.github.com>
2022-03-04 11:44:55 +1300

    Add files via upload

--- 05_token_generator_v2.py
+++ 05_token_generator_v2.py
@@ -6,7 +6,7 @@ STARTING_BALANCE = 100
 
 balance = STARTING_BALANCE
 # Testing loop to generate 20 tokens
-for item in range(0,100):
+for item in range(0,10):
     chosen_num = random.randint(1,100)
     
     # adjust balance
@@ -16,10 +16,11 @@ for item in range(0,100):
     elif 6 <= chosen_num <=36:
         balance -=1
     else:
-        if chosen_num = 2 == 0:
-        balance -=0.5
+        if chosen_num % 2 == 0:
+            chosen = "horse"
+        else:
+            chosen = "zebra"
+            balance -= 0.5
 
-# output
-print("Starting Balance: ${:.2f}".format(STARTING_BALANCE))
-print("Final Balance: ${:.2f}".format(balance))
-
+    # output
+    print("You got a {}.  Your balance is $""{:.2f}".format(chosen, balance))
